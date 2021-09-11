bd583062e3c118edf131e0a52648a2e0dbf7b2de
vinodhp <vinodhpolamarasetty@gmail.com>
2021-09-11 19:23:29 +0530

    added_static_root

--- Vaapas/settings.py
+++ Vaapas/settings.py
@@ -134,8 +134,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/var/www/vaapas-trail.herokuapp.com/static/"
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- Vaapas/urls.py
+++ Vaapas/urls.py
@@ -20,6 +20,10 @@ from users.views import *
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 schema_view = get_schema_view(
@@ -51,4 +55,6 @@ urlpatterns = [
 
     path('api-auth', include('rest_framework.urls')),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += staticfiles_urlpatterns()
