e7410749baf770b904d5796beedd86fb4663ed08
vinodhp <vinodhpolamarasetty@gmail.com>
2021-09-11 19:02:04 +0530

    added_views

--- Vaapas/urls.py
+++ Vaapas/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from users.views import *
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_swagger.views import get_swagger_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
