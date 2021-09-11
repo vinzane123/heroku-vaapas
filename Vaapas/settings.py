2ce7503fb6e2791856b22871f63ec0e283172b6f
vinodhp <vinodhpolamarasetty@gmail.com>
2021-09-11 19:39:53 +0530

    added_super_user

--- Vaapas/settings.py
+++ Vaapas/settings.py
@@ -135,13 +135,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = "/var/www/vaapas-trail.herokuapp.com/static/"
-
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/var/www/static/',
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
